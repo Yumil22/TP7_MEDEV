bddfd2d9f21c66a67f1d7c12944a5f5dd5ad5881
Anne-Sophie1497 <anne-sophie.cramilly@eleves.ec-nantes.fr>
2022-01-27 10:42:24 +0100

    Commentaires

--- DCT_converter.h
+++ DCT_converter.h
@@ -11,10 +11,15 @@ using namespace cv;
 class DCT_Converter{
 
 private:
+    /// Matrice dct_matrix de taille NxN (8x8) qui correspond à la matrice de la transformée DCT noté P dans le sujet
     Mat dct_matrix;
+    /// Matrice dct_matrix de taille NxN (8x8) qui correspond à la transposée de la matrice de la transformée DCT noté tP dans le sujet
     Mat t_dct_matrix;
+    /// Matrice rgb_to_yiq de taille 3x3 qui permet de passer du mode RGB au mode YIQ
     Mat rgb_to_yiq;
+    /// Matrice yiq_to_rgb de taille 3x3 qui permet de passer du mode YIQ au mode RGB
     Mat yiq_to_rgb;
+    /// Entier constant qui définit la taille des blocs (ici 8)
     const int N = 8;
 
     Mat openBMP(string fileName);
