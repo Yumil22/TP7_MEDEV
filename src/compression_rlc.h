c4eb746778aae4f72ad6d661dbf0de8862a9b10d
Brice Conraux <conrauxbrice@gmail.com>
2022-01-27 11:33:58 +0100

    documentation doxygen

--- src/compression_rlc.h
+++ src/compression_rlc.h
@@ -12,10 +12,25 @@
 #include <string>
 #include <vector>
 
+/// <summary>
+/// Compresse une matrice DTC 8*8 avec la methode RLC
+/// </summary>
+/// <param name="im"></param>
+/// <returns></returns>
 std::string compressionrlc(cv::Mat im);
 
+/// <summary>
+/// Lis une matrice DTC 8*8 et la stocke sous forme d'un vecteur simple (64 pixels * 3 canaux (RGB))
+/// </summary>
+/// <param name="im"></param>
+/// <returns></returns>
 std::vector<int> lecture(cv::Mat im);
 
+/// <summary>
+/// Compresse un vecteur d'int en un string avec la methode RLC
+/// </summary>
+/// <param name="vec"></param>
+/// <returns></returns>
 std::string comp(std::vector<int> vec);
 
 
