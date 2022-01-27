d490ad47c1f48bd656309ad3ca074fcf37aa7f20
Chongan <wca0404@gmail.com>
2022-01-27 09:42:29 +0100

    ecrit .h

---
+++ DTC_converter.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <opencv4/opencv2/opencv.hpp>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+
+
+class DCT_Converter{
+
+private:
+    Mat dct_matrix;
+    Mat t_dct_matrix;
+    Mat tgb_to_yiq;
+    Mat yiq_to_rgb;
+    const int N = 8;
+
+    Mat openBMP(string fileName);
+    Mat YIQ_conversion(Mat rgb_matrix);
+    Mat RGB_conversion(Mat yiq_matrix);
+    vector<Mat> separateMatrix(Mat yiq_matrix);
+    Mat recombineMatrix(vector<Mat> split_matrix);
+
+public:
+    vector<Mat> getDCTMatrixConverted(string fileName);
+    Mat deconvert(vector<Mat> dequantified_matrix);
+};
