47e5f45e13d98b28cff546dd985f984cbdbde81a
FranLal <francoislal@hotmail.fr>
2022-01-27 11:36:24 +0100

    Ajout de la methode de decodage de Huffman

--- src/Huffmann.cpp
+++ src/Huffmann.cpp
@@ -1,4 +1,7 @@
-﻿#include "huff_tree.h"
+#include <iostream>
+#include <vector>
+#include "huff_tree.h"
+using namespace std;
 
 Huffmann::Huffmann(std::string) {
 	treeRoot = new Huff_tree();
@@ -13,4 +16,29 @@ Huff_tree* Huffmann::getTreeRoot() {
 
 std::vector<std::byte> Huffmann::getByteCodes() {
 	return compressedMessage;
+}
+
+string decodage_huff(vector<bool> list, Huff_tree* root){
+    string res;
+    int n = list.size();
+
+    Huff_tree* ptr = root;
+
+    int i=0;
+
+    while(i<n){
+        if(ptr->fils0 == nullptr || ptr->fils1 == nullptr){
+            res.push_back(ptr->val);
+            ptr = root;
+        }
+        else{
+            if(list[i])
+                ptr = ptr->fils1;
+            else
+                ptr = ptr->fils0;
+        }
+        i++;
+    }
+
+    return res;
 }
