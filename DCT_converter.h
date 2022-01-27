34b025e7719da648d987cff0c9d4d5bc3f291b14
Chongan <wca0404@gmail.com>
2022-01-27 10:48:21 +0100

    Ajout de commentaires

--- DCT_converter.h
+++ DCT_converter.h
@@ -11,16 +11,31 @@ using namespace cv;
 class DCT_Converter{
 
 private:
+    // Matrix P de taille NxN
     Mat dct_matrix;
+    // Transform de Matrix P
     Mat t_dct_matrix;
+
+    // Matrix de transformation entre image RGB et YIQ
     Mat rgb_to_yiq;
     Mat yiq_to_rgb;
+
+    // Taille de DCT
     const int N = 8;
 
+    // Compression : Lecture de fichier image (BMP)
     Mat openBMP(string fileName);
+
+    // Compression : Convertir image RGB à YIQ
     Mat YIQ_conversion(Mat rgb_matrix);
+
+    // Décompresiion : Convertir image YIQ à RGB
     Mat RGB_conversion(Mat yiq_matrix);
+
+    // Compression : Découper matrice principale en taille NxN
     vector<Mat> separateMatrix(Mat yiq_matrix);
+
+    // Décompression : Regrouper les matrices séparées
     Mat recombineMatrix(vector<Mat> split_matrix);
 
 public:
